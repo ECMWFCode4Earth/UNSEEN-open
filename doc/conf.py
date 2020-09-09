a0ae3eee21144d307c4ced739ff526776087ba8a
timo <cenv0732@linux11.ouce.ox.ac.uk>
2020-09-09 11:22:49 +0100

    move to doc

--- doc/conf.py
+++ doc/conf.py
@@ -12,8 +12,10 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../Notebooks'))
+sys.path.insert(0, os.path.abspath('../Notebooks/'))
 
+# -- Import the Sphinx theme -------------------------------------------------
+import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
 
@@ -30,6 +32,7 @@ author = 'Timo Kelder'
 extensions = [
     'nbsphinx',
     'sphinx.ext.mathjax',
+    'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -46,7 +49,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
--- doc/index.rst
+++ doc/index.rst
@@ -10,7 +10,7 @@ Welcome to unseen_open's documentation!
    :maxdepth: 2
    :caption: Contents:
    
-   Workflow
+   Notebooks/Workflow
 
 
 
