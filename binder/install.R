9660afab2466123c4f1cd374c61b8a6b6819b982
timokelder <timo.kelder@gmail.com>
2020-10-01 16:22:48 +0200

    binder setup

---
+++ binder/install.R
@@ -0,0 +1,10 @@
+### install regular packages
+
+# install.packages("reticulate") # python support in RMarkdown
+# install.packages("ggplot2") # for plotting
+# install.packages(c("rmarkdown", "caTools", "bitops")) # for knitting
+
+
+### install GitHub packages (tag = commit, branch or release tag)
+install.packages("devtools")
+devtools::install_github("timokelder/UNSEEN")
