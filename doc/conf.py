7afe74833dddea2f83a5ff4053ed3e2e644b1fa7
timokelder <timo.kelder@gmail.com>
2020-10-07 13:10:14 +0200

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('../Notebooks/'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../src'))
 
 # -- Import the Sphinx theme -------------------------------------------------
 import sphinx_rtd_theme
