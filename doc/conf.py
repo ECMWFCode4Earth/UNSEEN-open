52ec138bb0dc2cfb7bedf318c4a41597e75872f3
timokelder <timo.kelder@gmail.com>
2020-10-07 13:59:35 +0200

    update requirements

--- doc/conf.py
+++ doc/conf.py
@@ -13,7 +13,6 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
-import src
 
 # -- Import the Sphinx theme -------------------------------------------------
 import sphinx_rtd_theme
