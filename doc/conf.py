70b160b2e49a395ecdfa8ef1d5dff648399dbc31
timokelder <timo.kelder@gmail.com>
2020-10-07 14:24:24 +0200

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../src'))
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- Import the Sphinx theme -------------------------------------------------
 import sphinx_rtd_theme
