11027eaa777e7d40cdd4e1eadc0e818ec41ebbb3
timo <cenv0732@linux11.ouce.ox.ac.uk>
2021-09-10 15:50:24 +0100

    upd plotfunc

--- src/evt_plot.r
+++ src/evt_plot.r
@@ -68,95 +68,164 @@ RV_ci <- function(extremes,covariate,return_period,covariate_values,GEV_type) { 
   return(rvs)
 }
 
-unseen_trends1 <- function(ensemble, obs, x_ens, x_obs, covariate_ens, covariate_obs, rp, GEV_type, title = "", ylab = 'Three-day precipitation (mm)') {
-
-    ci_ens=RV_ci(extremes = ensemble,
-                 covariate = covariate_ens,
-                 return_period = rp,
-                 covariate_values = covariate_obs, ## The covariate values are used to create the parameter matrix. This should only be the years, most likely same as covariate_obs. c(1:36 in this case)
-                 GEV_type=GEV_type)
-
-    ci_obs=RV_ci(extremes = obs,
-                 covariate = covariate_obs,
-                 return_period = rp,
-                 covariate_values = covariate_obs,
-                 GEV_type=GEV_type)
-
-    ###plot
-    cols=c("OBS"="blue","UNSEEN-trends"="orange")
-    p_trend=
-      ggplot()+
-      ggtitle(title) +
-      geom_point(aes(x = x_ens,y = ensemble),size=2,alpha=0.051)+
-       # scale_size_manual(values = seq(0.1,3,length.out = 3499)) +
-      geom_point(aes(x=x_obs,y = obs),col='blue',shape=4,size=2,stroke=1.5)+
-      geom_line(aes(x=x_obs,y = ci_ens[,2]),col='orange')+
-      geom_ribbon(aes(x=x_obs,ymin = ci_ens[,1],ymax=ci_ens[,3],fill="UNSEEN-trends"),alpha=0.5)+
-      geom_line(aes(x=x_obs,y = ci_obs[,2]),col='blue')+
-      geom_ribbon(aes(x=x_obs,ymin = ci_obs[,1],ymax=ci_obs[,3],fill="OBS"),alpha=0.2)+
-      theme_classic()+
-      ylab(ylab)+
-      scale_fill_manual(name="Method",values=cols) +
-      theme(axis.title.x=element_blank(),
-            plot.title = element_text(hjust = 0.5),
-            text=element_text(size=14),
-            axis.text = element_text(size=14))
+#This function was written specifically for the California case study. The year 2020 was left out of the fit for the observations.
+unseen_trends1 <- function (ensemble, obs, x_ens, x_obs, covariate_ens, covariate_obs, covariate_values,
+    rp, GEV_type, title = "", ylab = "Three-day precipitation (mm)") 
+{
+    ci_ens = RV_ci(extremes = ensemble, covariate = covariate_ens, 
+        return_period = rp, covariate_values = covariate_values, 
+        GEV_type = GEV_type)
+    ci_obs = RV_ci(extremes = obs[1:(length(obs)-1)], covariate = covariate_obs, 
+        return_period = rp, covariate_values = covariate_values, 
+        GEV_type = GEV_type)
+    cols = c(OBS = "blue", `UNSEEN-trends` = "orange")
+    p_trend = ggplot() + ggtitle(title) + geom_point(aes(x = x_ens, 
+        y = ensemble), size = 2, alpha = 0.051) + geom_point(aes(x = x_obs, 
+        y = obs), col = "blue", shape = 4, size = 2, stroke = 1.5) + 
+        geom_line(aes(x = x_obs, y = ci_ens[, 2]), col = "orange") + 
+        geom_ribbon(aes(x = x_obs, ymin = ci_ens[, 1], ymax = ci_ens[, 
+            3], fill = "UNSEEN-trends"), alpha = 0.5) + geom_line(aes(x = x_obs, 
+        y = ci_obs[, 2]), col = "blue") + geom_ribbon(aes(x = x_obs, 
+        ymin = ci_obs[, 1], ymax = ci_obs[, 3], fill = "OBS"), 
+        alpha = 0.2) + theme_classic() + ylab(ylab) + scale_fill_manual(name = "Method", 
+        values = cols) + theme(axis.title.x = element_blank(), 
+        plot.title = element_text(hjust = 0.5), text = element_text(size = 10), 
+        axis.text = element_text(size = 10))
     return(p_trend)
 }
 
+# unseen_trends1 <- function(ensemble, obs, x_ens, x_obs, covariate_ens, covariate_obs, rp, GEV_type, title = "", ylab = 'Three-day precipitation (mm)') {
 
-unseen_trends2 <- function(ensemble, obs, covariate_ens, covariate_obs, GEV_type, ylab = 'Three-day precipitation (mm)') {
+#     ci_ens=RV_ci(extremes = ensemble,
+#                  covariate = covariate_ens,
+#                  return_period = rp,
+#                  covariate_values = covariate_obs, ## The covariate values are used to create the parameter matrix. This should only be the years, most likely same as covariate_obs. c(1:36 in this case)
+#                  GEV_type=GEV_type)
 
-    # Calculate the return values for 1981 (covariate = 1)
-    rvs_1981=RV_ci(extremes = ensemble,
-                   covariate = covariate_ens,
-                   return_period = rperiods,
-                   covariate_values = 1,
-                   GEV_type = GEV_type) ##calc the return values
-    colnames(rvs_1981) = c('S5_1981_l','S5_1981','S5_1981_h','S5_1981_sd') #Rename the column
+#     ci_obs=RV_ci(extremes = obs,
+#                  covariate = covariate_obs,
+#                  return_period = rp,
+#                  covariate_values = covariate_obs,
+#                  GEV_type=GEV_type)
 
-    # Calculate the return values for 2015 (covariate = 35)
-    rvs_2015=RV_ci(extremes = ensemble,
-                   covariate = covariate_ens,
-                   return_period = rperiods,
-                   covariate_values = 35,
-                   GEV_type = GEV_type)
-    colnames(rvs_2015) = c('S5_2015_l','S5_2015','S5_2015_h','S5_2015_sd')
+#     ###plot
+#     cols=c("OBS"="blue","UNSEEN-trends"="orange")
+#     p_trend=
+#       ggplot()+
+#       ggtitle(title) +
+#       geom_point(aes(x = x_ens,y = ensemble),size=2,alpha=0.051)+
+#        # scale_size_manual(values = seq(0.1,3,length.out = 3499)) +
+#       geom_point(aes(x=x_obs,y = obs),col='blue',shape=4,size=2,stroke=1.5)+
+#       geom_line(aes(x=x_obs,y = ci_ens[,2]),col='orange')+
+#       geom_ribbon(aes(x=x_obs,ymin = ci_ens[,1],ymax=ci_ens[,3],fill="UNSEEN-trends"),alpha=0.5)+
+#       geom_line(aes(x=x_obs,y = ci_obs[,2]),col='blue')+
+#       geom_ribbon(aes(x=x_obs,ymin = ci_obs[,1],ymax=ci_obs[,3],fill="OBS"),alpha=0.2)+
+#       theme_classic()+
+#       ylab(ylab)+
+#       scale_fill_manual(name="Method",values=cols) +
+#       theme(axis.title.x=element_blank(),
+#             plot.title = element_text(hjust = 0.5),
+#             text=element_text(size=14),
+#             axis.text = element_text(size=14))
+#     return(p_trend)
+# }
 
-    rvs_obs_1981=RV_ci(extremes = obs,
-                       covariate = covariate_obs,
-                       return_period = rperiods,
-                       covariate_values = 1,
-                       GEV_type = GEV_type)
-    colnames(rvs_obs_1981) = c('Obs_1981_l','Obs_1981','Obs_1981_h','Obs_1981_sd') #Rename the col
 
-    rvs_obs_2015=RV_ci(extremes = obs,
-                       covariate = covariate_obs,
-                       return_period = rperiods,
-                       covariate_values = 35,
-                       GEV_type = GEV_type)
-    colnames(rvs_obs_2015) = c('Obs_2015_l','Obs_2015','Obs_2015_h','Obs_2015_sd')
-
-    ## Combine the return values into one dataframe
-    rvs=data.frame(cbind(rvs_1981,rvs_2015,rvs_obs_1981,rvs_obs_2015,rperiods))
-
-#     cols=c("S5_1981"="#f04546","S5_2015"="#3591d1","Obs_1981"="#62c76b","Obs_2015"="#62c76b")
-    cols=c("1981"="black","2015"="red")
-    p=ggplot(data = rvs,aes(x=rperiods))+
-      geom_line(aes(y = S5_1981),col='black')+
-      geom_ribbon(aes(ymin=S5_1981_l,ymax=S5_1981_h,fill='1981'),alpha=0.5)+
-      geom_line(aes(y = S5_2015,colour="2015"),col='red')+
-      geom_ribbon(aes(ymin=S5_2015_l,ymax=S5_2015_h,fill="2015"), alpha=0.5)+
-      geom_line(aes(y = Obs_1981),col='black',linetype ='dashed')+
-      geom_ribbon(aes(ymin=Obs_1981_l,ymax=Obs_1981_h),fill='black', alpha=0.1)+
-      geom_line(aes(y = Obs_2015),col='red',linetype ='dashed')+
-      geom_ribbon(aes(ymin=Obs_2015_l,ymax=Obs_2015_h),fill='red', alpha=0.1)+
-      scale_x_continuous(trans='log10')+
-      theme_classic()+
-      scale_fill_manual(name="Years",values=cols) +
-      xlab('Return period (years)')+
-      ylab(ylab)+
-      theme(text=element_text(size=14),
-            axis.text = element_text(size=14))
+unseen_trends2 <- function (ensemble, obs, covariate_ens, covariate_obs, GEV_type, 
+                            covariate_values = c(1, length(obs)),
+                            rperiods = c(2, 5, 10, 20, 50, 80, 100, 120, 200, 250, 300, 500, 800,2000,5000),
+                            ylab = "Return value") 
+{
+    rvs_1981 = RV_ci(extremes = ensemble, covariate = covariate_ens, 
+        return_period = rperiods, covariate_values = covariate_values[1], GEV_type = GEV_type)
+    colnames(rvs_1981) = c("S5_1981_l", "S5_1981", "S5_1981_h", 
+        "S5_1981_sd")
+    rvs_2020 = RV_ci(extremes = ensemble, covariate = covariate_ens, 
+        return_period = rperiods, covariate_values = covariate_values[2], GEV_type = GEV_type)
+    colnames(rvs_2020) = c("S5_2020_l", "S5_2020", "S5_2020_h", 
+        "S5_2020_sd")
+    rvs_obs_1981 = RV_ci(extremes = obs, covariate = covariate_obs, 
+        return_period = rperiods, covariate_values = covariate_values[1], GEV_type = GEV_type)
+    colnames(rvs_obs_1981) = c("Obs_1981_l", "Obs_1981", "Obs_1981_h", 
+        "Obs_1981_sd")
+    rvs_obs_2020 = RV_ci(extremes = obs, covariate = covariate_obs, 
+        return_period = rperiods, covariate_values = covariate_values[2], GEV_type = GEV_type)
+    colnames(rvs_obs_2020) = c("Obs_2020_l", "Obs_2020", "Obs_2020_h", 
+        "Obs_2020_sd")
+    rvs = data.frame(cbind(rvs_1981, rvs_2020, rvs_obs_1981, 
+        rvs_obs_2020, rperiods))
+    cols = c(`1981` = "black", `2020` = "red")
+    p = ggplot(data = rvs, aes(x = rperiods)) + 
+    geom_line(aes(y = S5_1981),col = "black") + 
+    geom_ribbon(aes(ymin = S5_1981_l, ymax = S5_1981_h,fill = "1981"), alpha = 0.5) + 
+    geom_line(aes(y = S5_2020,colour = "2020"), col = "red") + 
+    geom_ribbon(aes(ymin = S5_2020_l,ymax = S5_2020_h, fill = "2020"), alpha = 0.5) +
+    geom_line(aes(y = Obs_1981),col = "black", linetype = "dashed") +
+    geom_ribbon(aes(ymin = Obs_1981_l,ymax = Obs_1981_h), fill = "black", alpha = 0.1) +
+    geom_line(aes(y = Obs_2020),col = "red", linetype = "dashed") +
+    geom_ribbon(aes(ymin = Obs_2020_l,ymax = Obs_2020_h), fill = "red", alpha = 0.1) +
+    scale_x_continuous(trans = "log10") +
+    theme_classic() +
+    scale_fill_manual(name = "Years", values = cols) +
+    xlab("Return period (years)") +
+    ylab(ylab) +
+    theme(text = element_text(size = 10),
+          axis.text = element_text(size = 10))
     return(p)
 }
+
+# unseen_trends2 <- function(ensemble, obs, covariate_ens, covariate_obs, GEV_type, ylab = 'Three-day precipitation (mm)') {
+
+#     # Calculate the return values for 1981 (covariate = 1)
+#     rvs_1981=RV_ci(extremes = ensemble,
+#                    covariate = covariate_ens,
+#                    return_period = rperiods,
+#                    covariate_values = 1,
+#                    GEV_type = GEV_type) ##calc the return values
+#     colnames(rvs_1981) = c('S5_1981_l','S5_1981','S5_1981_h','S5_1981_sd') #Rename the column
+
+#     # Calculate the return values for 2015 (covariate = 35)
+#     rvs_2015=RV_ci(extremes = ensemble,
+#                    covariate = covariate_ens,
+#                    return_period = rperiods,
+#                    covariate_values = 35,
+#                    GEV_type = GEV_type)
+#     colnames(rvs_2015) = c('S5_2015_l','S5_2015','S5_2015_h','S5_2015_sd')
+
+#     rvs_obs_1981=RV_ci(extremes = obs,
+#                        covariate = covariate_obs,
+#                        return_period = rperiods,
+#                        covariate_values = 1,
+#                        GEV_type = GEV_type)
+#     colnames(rvs_obs_1981) = c('Obs_1981_l','Obs_1981','Obs_1981_h','Obs_1981_sd') #Rename the col
+
+#     rvs_obs_2015=RV_ci(extremes = obs,
+#                        covariate = covariate_obs,
+#                        return_period = rperiods,
+#                        covariate_values = 35,
+#                        GEV_type = GEV_type)
+#     colnames(rvs_obs_2015) = c('Obs_2015_l','Obs_2015','Obs_2015_h','Obs_2015_sd')
+
+#     ## Combine the return values into one dataframe
+#     rvs=data.frame(cbind(rvs_1981,rvs_2015,rvs_obs_1981,rvs_obs_2015,rperiods))
+
+# #     cols=c("S5_1981"="#f04546","S5_2015"="#3591d1","Obs_1981"="#62c76b","Obs_2015"="#62c76b")
+#     cols=c("1981"="black","2015"="red")
+#     p=ggplot(data = rvs,aes(x=rperiods))+
+#       geom_line(aes(y = S5_1981),col='black')+
+#       geom_ribbon(aes(ymin=S5_1981_l,ymax=S5_1981_h,fill='1981'),alpha=0.5)+
+#       geom_line(aes(y = S5_2015,colour="2015"),col='red')+
+#       geom_ribbon(aes(ymin=S5_2015_l,ymax=S5_2015_h,fill="2015"), alpha=0.5)+
+#       geom_line(aes(y = Obs_1981),col='black',linetype ='dashed')+
+#       geom_ribbon(aes(ymin=Obs_1981_l,ymax=Obs_1981_h),fill='black', alpha=0.1)+
+#       geom_line(aes(y = Obs_2015),col='red',linetype ='dashed')+
+#       geom_ribbon(aes(ymin=Obs_2015_l,ymax=Obs_2015_h),fill='red', alpha=0.1)+
+#       scale_x_continuous(trans='log10')+
+#       theme_classic()+
+#       scale_fill_manual(name="Years",values=cols) +
+#       xlab('Return period (years)')+
+#       ylab(ylab)+
+#       theme(text=element_text(size=14),
+#             axis.text = element_text(size=14))
+#     return(p)
+# }
